b6e45f7f64e29d535da589b97af98c24fd6a1bb0
numi2 <numit22@gmail.com>
2026-07-27 00:59:03 +0200

    Frame shared needle exchange in video

--- tests/test_dranmar_learning_path.py
+++ tests/test_dranmar_learning_path.py
@@ -536,6 +536,7 @@ def test_block_lift_requires_physics_owned_height_and_sustained_contact() -> Non
     assert '"environments_with_four_jaw_overlap_contact"' in benchmark_source
     assert '"successful_environment_indices"' in benchmark_source
     assert 'handover_sweep.add_argument("--video", action="store_true")' in benchmark_source
+    assert 'env_cfg.viewer.origin_type = "env"' in benchmark_source
     assert "DR_ANMAR_HANDOVER_VIDEO_ENV_INDEX" in launcher_source
     assert '"goal_position_without_qualified_state"' in benchmark_source
     assert '"qualified_state_without_sustained_dwell"' in benchmark_source
