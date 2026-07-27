3cb152dc4d6658b1828f82e2e08f3d78674f1c60
numi2 <numit22@gmail.com>
2026-07-27 04:17:08 +0200

    Revert regressed handover presentation point

--- source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/handover/residual_model.py
+++ source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/handover/residual_model.py
@@ -40,7 +40,7 @@ class HandoverAnalyticController(nn.Module):
         self.slow_approach_action_limit = 0.1
         self.receiver_contact_centering_action_limit = 0.0025
         self.normalized_contact_threshold = 0.002
-        self.presentation_fraction_from_giver = 0.30
+        self.presentation_fraction_from_giver = 0.35
         self.presentation_height_in_robot_frame = -0.13
         self.presentation_ready_tolerance = 0.005
         self.minimum_lift_height_in_robot_frame = -0.139
