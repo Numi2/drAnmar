ead3095a38e16978d5bf4ad2f1816b624392c7d4
numi2 <numit22@gmail.com>
2026-07-29 21:49:37 +0200

    Require receiver contact before custody retry

--- source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/handover/recovery_policy.py
+++ source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/handover/recovery_policy.py
@@ -3243,6 +3243,7 @@ class HandoverReceiverRecoveryPolicy(nn.Module):
         giver_degradation_observed = (
             acquisition_active
             & ~bilateral_qualified
+            & any_contact
             & giver_any_contact
             & ~giver_bilateral_live
         )
