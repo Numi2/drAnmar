429a7c800ea2831033af92ef69fab2aae047a393
numi2 <numit22@gmail.com>
2026-07-29 21:29:49 +0200

    Center only unilateral receiver contact

--- source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/handover/recovery_policy.py
+++ source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/handover/recovery_policy.py
@@ -3496,6 +3496,7 @@ class HandoverReceiverRecoveryPolicy(nn.Module):
             & (phase == 3)
             & ~giver_any_contact
             & any_contact
+            & ~bilateral_live
         )
         retention_contact_centering_action = torch.zeros_like(
             receiver_hold_closed
